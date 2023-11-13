63507483699fa40e6d7ee64cfc34c9df4f0cbab9
Jonathan Song <jonats10@uci.edu>
2023-11-13 11:32:09 -0800

    finish jupiter years test green phase

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -4,5 +4,6 @@ export class ageCalculator {
     this.mercury = +(age/0.24).toFixed(2)
     this.venus = +(age/0.62).toFixed(2)
     this.mars = +(age/1.88).toFixed(2)
+    this.jupiter = +(age/11.86).toFixed(2)
   }
 }
