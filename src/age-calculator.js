dff2b45f52fb89d27940954dfecf75201ff44169
Jonathan Song <jonats10@uci.edu>
2023-11-13 16:49:34 -0800

    wip ui logic

--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,29 +1,41 @@
 export class ageCalculator {
   constructor(age) {
-    this.earth = age
-    this.mercury = +(age/0.24).toFixed(2)
-    this.venus = +(age/0.62).toFixed(2)
-    this.mars = +(age/1.88).toFixed(2)
-    this.jupiter = +(age/11.86).toFixed(2)
+    this.earth = age;
+    this.mercury = +(age / 0.24).toFixed(2);
+    this.venus = +(age / 0.62).toFixed(2);
+    this.mars = +(age / 1.88).toFixed(2);
+    this.jupiter = +(age / 11.86).toFixed(2);
   }
 
   yearsSinceBday(bday) {
-    const earthDiff = this.earth-bday
-    const mercuryDiff = +(earthDiff/.24).toFixed(2)
-    const venusDiff = +(earthDiff/.62).toFixed(2)
-    const marsDiff = +(earthDiff/1.88).toFixed(2)
-    const jupiterDiff = +(earthDiff/11.86).toFixed(2)
+    const earthDiff = this.earth - bday;
+    const mercuryDiff = +(earthDiff / 0.24).toFixed(2);
+    const venusDiff = +(earthDiff / 0.62).toFixed(2);
+    const marsDiff = +(earthDiff / 1.88).toFixed(2);
+    const jupiterDiff = +(earthDiff / 11.86).toFixed(2);
 
-    return [{earth:earthDiff},{mercury:mercuryDiff},{venus:venusDiff},{mars:marsDiff}, {jupiter:jupiterDiff}]
+    return [
+      { earth: earthDiff },
+      { mercury: mercuryDiff },
+      { venus: venusDiff },
+      { mars: marsDiff },
+      { jupiter: jupiterDiff },
+    ];
   }
 
   yearsTillBday(bday) {
-    const earthDiff = bday-this.earth
-    const mercuryDiff = +(earthDiff/.24).toFixed(2)
-    const venusDiff = +(earthDiff/.62).toFixed(2)
-    const marsDiff = +(earthDiff/1.88).toFixed(2)
-    const jupiterDiff = +(earthDiff/11.86).toFixed(2)
+    const earthDiff = bday - this.earth;
+    const mercuryDiff = +(earthDiff / 0.24).toFixed(2);
+    const venusDiff = +(earthDiff / 0.62).toFixed(2);
+    const marsDiff = +(earthDiff / 1.88).toFixed(2);
+    const jupiterDiff = +(earthDiff / 11.86).toFixed(2);
 
-    return [{earth:earthDiff},{mercury:mercuryDiff},{venus:venusDiff},{mars:marsDiff},{jupiter:jupiterDiff}]
+    return [
+      { earth: earthDiff },
+      { mercury: mercuryDiff },
+      { venus: venusDiff },
+      { mars: marsDiff },
+      { jupiter: jupiterDiff },
+    ];
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import "./css/styles.css";
-alert("hello");
+import { ageCalculator } from "./../src/age-calculator.js";
+
+window.addEventListener("load", () => {
+  document
+    .querySelector("#form")
+    .addEventListener("submit", handleFormSubmission);
+});
+
+function handleFormSubmission(e) {
+  e.preventDefault();
+  const age = document.querySelector("#age").value;
+  const newPerson = ageCalculator(age);
+  new planetList = [newPerson.earth,newPerson.mercury,newPerson.venus,newPerson.mars,newPerson.jupiter]
+  planetList.forEach((year)=> {
+    const paragraph = document.createElement("p")
+    paragraph.append("Age in")
+  })
+
+}
