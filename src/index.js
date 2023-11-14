2645a1155975fa029b24a86fbf5379fc8af6991a
Jonathan Song <jonats10@uci.edu>
2023-11-13 17:02:36 -0800

    final changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import "./css/styles.css";
-import { ageCalculator } from "./../src/age-calculator.js";
 
-window.addEventListener("load", () => {
-  document
-    .querySelector("#form")
-    .addEventListener("submit", handleFormSubmission);
-});
 
-function handleFormSubmission(e) {
-  e.preventDefault();
-  const age = document.querySelector("#age").value;
-  const newPerson = ageCalculator(age);
-  new planetList = [newPerson.earth,newPerson.mercury,newPerson.venus,newPerson.mars,newPerson.jupiter]
-  planetList.forEach((year)=> {
-    const paragraph = document.createElement("p")
-    paragraph.append("Age in")
-  })
 
-}
+
+
+
+  
+
+
